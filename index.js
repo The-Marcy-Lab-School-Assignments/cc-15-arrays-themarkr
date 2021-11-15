0c1d68399cc1673dcbe6901ad5195621c5315b96
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-11-15 15:05:11 +0000

    code challenge completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //write your code here
+const findLongestWord = array => {
+    let longestWord = array[0];
+    for (let i = 1; i < array.length; i++){
+        if (array[i].length > longestWord.length){
+            longestWord = array[i];
+        }
+    }
+    return longestWord;
+};
+
+console.log(findLongestWord(["The", "quick", "brown","fox","jumped","over","the","lazy","dog"]))
